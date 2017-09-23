bb9b207d81cef1e550232fe54a79c19a55fd2e90
Alberto Cantú Gómez <betocantu93@gmail.com>
2017-09-22 23:52:26 -0500

    new

---
+++ 06_constants/01_constant/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+const p = "death"
+const q = 42
+
+func main() {
+
+	fmt.Println("p - ", p)
+	fmt.Println("q - ", q)
+
+}
---
+++ 06_constants/02_multiple-initialization/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+const (
+	q = "Hola"
+	x = "Bye"
+)
+
+func main() {
+	fmt.Println("q", q)
+	fmt.Println("x", x)
+}
