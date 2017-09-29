9ebca92583ab8d2dc03583940bb31a5d8b2fd1d4
Alberto Cantú Gómez <betocantu93@gmail.com>
2017-09-29 00:32:24 -0500

    Arrays

---
+++ 17_array/05/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var x [256]string
+
+	for i := 0; i < 256; i++ {
+		x[i] = string(i)
+	}
+
+	for _, v := range x {
+		fmt.Printf("%v - %T - %b \n", v, v, []byte(v))
+	}
+
+}
